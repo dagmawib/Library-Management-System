23add4c3415d2472c45ac1342b10e03313648546
Eeyban <abaytesfaye1219@gmail.com>
2023-12-20 06:39:42 -0800

    adding borrower name and returner name

--- src/MainGUI.java
+++ src/MainGUI.java
@@ -12,40 +12,36 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-
 public class MainGUI {
     private JFrame frame;
     private JTextField searchTextField;
     private JLabel searchResultLabel;
     private JTextField borrowTextField;
-    private JTextField borrowerNameTextField;
+    private JTextField borrowerNameTextField; // Added for borrower name
     private JLabel borrowResultLabel;
     private JTextField returnTextField;
-    private JTextField returnerNameTextField;
+    private JTextField returnerNameTextField; // Added for returner name
     private JLabel returnResultLabel;
 
     public static void main(String[] args) {
-
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-//                new MainGUI().loginGUI();
+                // new MainGUI().loginGUI();
             }
         });
     }
 
-     MainGUI(String userID) {
+    MainGUI(String userID) {
         frame = new JFrame("Library Management System");
         JLabel MainPage = new JLabel("Hello!");
-         frame.setBounds(0,0,200,35);
-         frame.setFont(new Font(null,Font.PLAIN,25));
-//         frame.setText("Hello "+userID);
+        frame.setBounds(0, 0, 200, 35);
+        frame.setFont(new Font(null, Font.PLAIN, 25));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(420,420);
+        frame.setSize(420, 420);
         frame.setResizable(true);
         frame.setVisible(true);
         ImageIcon image = new ImageIcon("logo.png");
         frame.setIconImage(image.getImage());
-
 
         JPanel panel = new JPanel(new BorderLayout());
 
@@ -67,8 +63,12 @@ public class MainGUI {
         borrowPanel.add(new JLabel("Borrow Book:"));
         borrowTextField = new JTextField(20);
         borrowPanel.add(borrowTextField);
+
+        // Add a label and text field for borrower name
+        borrowPanel.add(new JLabel("Borrower Name:"));
         borrowerNameTextField = new JTextField(20);
         borrowPanel.add(borrowerNameTextField);
+
         JButton borrowButton = new JButton("Borrow");
         borrowButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -83,23 +83,13 @@ public class MainGUI {
         returnPanel.add(new JLabel("Return Book:"));
         returnTextField = new JTextField(20);
         returnPanel.add(returnTextField);
+
+        // Add a label and text field for returner name
+        returnPanel.add(new JLabel("Returner Name:"));
         returnerNameTextField = new JTextField(20);
         returnPanel.add(returnerNameTextField);
+
         JButton returnButton = new JButton("Return");
-        returnButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                returnBook();
-            }
-        });
-        returnPanel.add(returnButton);
-        returnResultLabel =new JLabel();
-        returnPanel = new JPanel();
-        returnPanel.add(new JLabel("Return Book:"));
-        returnTextField = new JTextField(20);
-        returnPanel.add(returnTextField);
-        returnerNameTextField = new JTextField(20);
-        returnPanel.add(returnerNameTextField);
-        returnButton = new JButton("Return");
         returnButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 returnBook();
