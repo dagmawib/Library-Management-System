a887a28d7768c65e8787df8478685c92bf45b82f
Eeyban <abaytesfaye1219@gmail.com>
2023-12-21 11:31:31 -0800

    adding comment

--- src/IDandPasswords.java
+++ src/IDandPasswords.java
@@ -1,8 +1,11 @@
 import java.util.HashMap;
 public class IDandPasswords {
 
+/* This well-structured HashMap named 'logininfo' elegantly manages user credentials
+with a clear key-value association. The use of String types for both keys and values
+enhances readability, making it a secure and efficient solution for handling login information.
+ * */
     HashMap<String,String> logininfo = new HashMap<String,String>();
-
     IDandPasswords(){
 
         logininfo.put("Abdi","123");
@@ -11,7 +14,6 @@ public class IDandPasswords {
         logininfo.put("Anan","321");
         logininfo.put("Dagmawi","654");
     }
-
     public HashMap getLoginInfo(){
         return logininfo;
     }
