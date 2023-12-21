40f62ec2c9e6465ea414e83e8b1865d801e22ccf
Eeyban <abaytesfaye1219@gmail.com>
2023-12-21 11:06:03 -0800

    Reduce code redundancy

--- src/AddBook.java
+++ src/AddBook.java
@@ -1,42 +1,36 @@
 import java.util.Scanner;
 import java.util.ArrayList;
-
 public class AddBook {
+    // adding private instance variable
         private int id;
         private String title;
         private String author;
         private boolean borrowed;
-
         public AddBook(String title, String author) {
             this.id = id;
             this.title = title;
             this.author = author;
             this.borrowed = borrowed;
         }
-
+        // setting getter method
         public int getId() {
             return id;
         }
-
         public String getTitle() {
             return title;
         }
-
         public String getAuthor() {
             return author;
         }
-
         public boolean isBorrowed() {
             return borrowed;
         }
-
         public void setBorrowed(boolean borrowed) {
             this.borrowed = borrowed;
         }
         public static AddBook main() {
             Scanner scanner = new Scanner(System.in);
             ArrayList<AddBook> books = new ArrayList<>();
-
             while (true) {
                 System.out.print("Enter book title (or type 'exit' to stop adding books): ");
                 String title = scanner.nextLine();
@@ -44,7 +38,6 @@ public class AddBook {
                 if (title.equalsIgnoreCase("exit")) {
                     break;
                 }
-
                 System.out.print("Enter book author: ");
                 String author = scanner.nextLine();
 
@@ -54,12 +47,10 @@ public class AddBook {
 
                 System.out.println("Book added: " + title + " by " + author);
             }
-
             System.out.println("\nList of Books Added:");
             for (AddBook book : books) {
                 System.out.println("Title: " + book.getTitle() + ", Author: " + book.getAuthor());
             }
-
             scanner.close();
             return null;
         }
