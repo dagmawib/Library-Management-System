f27c79703df52e92c1d0603d91d4e8760b306bbd
Eeyban <abaytesfaye1219@gmail.com>
2023-12-21 11:50:24 -0800

    code update

--- src/LibraryManagementSystem.java
+++ src/LibraryManagementSystem.java
@@ -1,14 +1,11 @@
 public class LibraryManagementSystem {
     private Library library;
-
     public LibraryManagementSystem() {
         this.library = new Library();
     }
-
     public Library getLibrary() {
         return library;
     }
-
     public void borrowBook(AddBook book, Member member) {
         if (!book.isBorrowed()) {
             book.setBorrowed(true);
@@ -17,7 +14,6 @@ public class LibraryManagementSystem {
             System.out.println("Book \"" + book.getTitle() + "\" is already borrowed.");
         }
     }
-
     public void returnBook(AddBook book, Member member) {
         if (book.isBorrowed()) {
             book.setBorrowed(false);
@@ -26,9 +22,7 @@ public class LibraryManagementSystem {
             System.out.println("Book \"" + book.getTitle() + "\" is not currently borrowed.");
         }
     }
-
-    public AddBook get() {
-
-        return null;
-    }
+//    public AddBook get() {
+//        return null;
+//    }
 }
--- src/Member.java
+++ src/Member.java
@@ -2,20 +2,16 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 public class Member {
     private String name;
     private String contactDetails;
-
     public Member(String name, String contactDetails) {
         this.name = name;
         this.contactDetails = contactDetails;
     }
-
     public String getName() {
         return name;
     }
-
     public String getContactDetails() {
         return contactDetails;
     }
