b0fc6eb061149fcf740e6d8ff7ecd46acf3c7e80
asm2212 <asmareadmasu0@gmail.com>
2023-12-24 08:32:18 +0300

    added comment => loginpage

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -7,6 +7,7 @@ import java.awt.event.FocusListener;
 import java.util.HashMap;
 import java.util.logging.*;
 public class LoginPage implements ActionListener {
+//    Declares instance variables for the components of the login page and a HashMap for storing login information.
     JFrame frame = new JFrame();
     JButton loginButton = new JButton("Login");
     JTextField userIDField = new JTextField();
@@ -15,17 +16,16 @@ public class LoginPage implements ActionListener {
     JLabel userPasswordLabel = new JLabel("Password:");
     JLabel messageLabel = new JLabel();
     JLabel logoLabel;  // New JLabel for the logo
-
    HashMap<String, String> logininfo = new HashMap<>();
 
 
     LoginPage(HashMap<String, String> loginInfoOriginal) {
         logininfo = loginInfoOriginal;
 
+//        Configures font styles and colors for various components.
         Font labelFont = userIDLabel.getFont();
         userIDLabel.setFont(new Font(labelFont.getName(), Font.PLAIN, labelFont.getSize() * 2));
         userPasswordLabel.setFont(new Font(labelFont.getName(), Font.PLAIN, labelFont.getSize() * 2));
-
         userIDLabel.setForeground(Color.white);
         userPasswordLabel.setForeground(Color.white);
 
@@ -47,7 +47,7 @@ public class LoginPage implements ActionListener {
             Logger logger = Logger.getLogger(LoginPage.class.getName());
             logger.log(Level.SEVERE, "An error occurred while loading the background image.", e);
         }
-
+//        Configures frame properties such as size, layout, and default close operation.
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(900, 800);
         frame.setLayout(new GridBagLayout());
@@ -63,6 +63,8 @@ public class LoginPage implements ActionListener {
                 ImageIcon logoImage = new ImageIcon(imageUrl);
                 logoImage.setImage(logoImage.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH)); // Adjust the logo size
                 logoLabel = new JLabel(logoImage);
+
+//                Attempts to load and set a logo image in the frame.
                 gbc.gridx = 1;
                 gbc.gridy = 0;  // Logo above the username
                 gbc.gridwidth = 2;
@@ -76,11 +78,12 @@ public class LoginPage implements ActionListener {
             Logger logger = Logger.getLogger(LoginPage.class.getName());
             logger.log(Level.SEVERE, "An error occurred while loading the logo image.", e);
         }
-
+//        Adds the username field to the frame.
         gbc.gridx = 0;
         gbc.gridy = 1;
         frame.add(userIDLabel, gbc);
 
+//        Adds the username field to the frame.
         gbc.gridx = 1;
         gbc.gridy = 1;
         gbc.gridwidth = 2;
@@ -89,17 +92,20 @@ public class LoginPage implements ActionListener {
         setPlaceholder(userIDField, "Enter username");
         frame.add(userIDField, gbc);
 
+//        Adds the password label to the frame.
         gbc.gridx = 0;
         gbc.gridy = 2;
         gbc.gridwidth = 1;
         frame.add(userPasswordLabel, gbc);
 
+//        Adds the password field to the frame.
         gbc.gridx = 1;
         gbc.gridy = 2;
         gbc.gridwidth = 2;
         userPasswordField.setPreferredSize(new Dimension(500, 30));
         frame.add(userPasswordField, gbc);
 
+  //      Adds the login button to the frame.
         gbc.gridx = 1;
         gbc.gridy = 3;
         gbc.gridwidth = 2;
@@ -108,16 +114,19 @@ public class LoginPage implements ActionListener {
         loginButton.setForeground(Color.WHITE);
         frame.add(loginButton, gbc);
 
+//        Adds the message label to the frame.
         gbc.gridx = 1;
         gbc.gridy = 4;
         gbc.gridwidth = 2;
         frame.add(messageLabel, gbc);
 
+//        Centers the frame on the screen.
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         int centerX = (int) ((screenSize.getWidth() - frame.getWidth()) / 2);
         int centerY = (int) ((screenSize.getHeight() - frame.getHeight()) / 2);
         frame.setLocation(centerX, centerY);
 
+//        Adjusts labels, sets focus properties, and adds an action listener to the login button.
         userIDLabel.setText("Username:");
         userPasswordLabel.setText("Password:");
         loginButton.setFocusable(false);
@@ -147,7 +156,7 @@ public class LoginPage implements ActionListener {
             }
         });
     }
-
+//    Implements the actionPerformed method required by the ActionListener interface, handling login attempts.
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == loginButton) {
@@ -172,12 +181,4 @@ public class LoginPage implements ActionListener {
         }
     }
 }
-//    public static void main(String[] args) {
-//        // Example usage:
-//        HashMap<String, String> loginInfo = new HashMap<>();
-//        loginInfo.put("user1", "password1");
-//        loginInfo.put("user2", "password2");
-//
-//        SwingUtilities.invokeLater(() -> new LoginPage(loginInfo));
-//    }
-//}
+
