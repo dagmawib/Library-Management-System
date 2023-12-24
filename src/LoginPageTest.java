92ab5f47d28b39d802db1f1a76cc5114aa2b52d4
AnanSer <ananserbesa@gmail.com>
2023-12-24 17:00:15 +0300

    updated Logout unit Test

--- src/LoginPageTest.java
+++ src/LoginPageTest.java
@@ -1,15 +1,18 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import java.util.HashMap;
+//this is our LoginPageTest class
+
 
 public class LoginPageTest {
 
     @Test
-    public void testHashMapInsertion() {
+    public void loginPageVerification() {
         HashMap<String, String> loginInfoOriginals = new HashMap<>();
+        //insert into loginInfoOriginals
         loginInfoOriginals.put("username", "Anan");
         loginInfoOriginals.put("password", "321");
-
+        // insert username and password in the assertions
         Assertions.assertEquals("Anan", loginInfoOriginals.get("username"));
         Assertions.assertEquals("321", loginInfoOriginals.get("password"));
     }
